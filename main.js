940a7eeabe4d18c48d06b3dc44ac9049f3283928
s24542-pj <92208963+s24542-pj@users.noreply.github.com>
2023-01-14 17:05:01 +0100

    Update with added javascript functions
    login, add post, delete post and other functions like displaying username

---
+++ main.js
@@ -0,0 +1,216 @@
+
+function Loginsave(){
+  var login = document.getElementById("login");
+  var password = document.getElementById("password");
+  localStorage.setItem("login", login.value);
+  localStorage.setItem("password", password.value);
+
+  window.open("author2.html","_self");
+
+}
+
+function Loginifitis(){
+
+  if(localStorage.getItem("login") === null){
+      window.open("author.html","_self");
+  }else{
+      window.open("author2.html","_self");
+      window.onload = Displaypost;
+  }
+}
+
+function Clear(){
+  localStorage.removeItem("login");
+  window.open("author.html","_self");
+}
+function username(){
+  var p = document.createElement('p');
+  p.classList.add('username')
+  p.innerHTML = " Welcome: "+ "<b>"+localStorage.getItem("login")+"</b>";
+
+  document.getElementById("usernameblock").appendChild(p);
+}
+
+function Getdata(){
+  var title = document.getElementById("title");
+  var inside = document.getElementById("inside");
+  var ulogin = localStorage.getItem("login");
+
+  const post = {
+    postlogin : ulogin,
+    posttitle : title.value,
+    postinside : inside.value
+  }
+  for (let i = 0; i < 9; i++){
+    if (localStorage.getItem(i) === null){
+      window.localStorage.setItem(i,JSON.stringify(post))
+      break;
+    } else {
+      continue;
+    }
+  }
+  window.open("author2.html","_self");
+}
+function Edittxt(){
+  var etitle = document.getElementById("title");
+  var einside = document.getElementById("inside");
+}
+
+function Displaypost() {
+
+  for (let i = 0; i < 9; i++){
+    if (localStorage.getItem(i) == null){
+      continue;
+    } else {
+      let log = localStorage.getItem(i);
+      let logg = JSON.parse(log);
+      var pos = document.createElement('div');
+      pos.className = 'row';
+      
+
+      pos.innerHTML =`
+        <h2>`+ logg.posttitle +`</h2>
+        <div class="editt">
+          
+        </div>
+        <p class="text">`+ logg.postinside +`</p>
+        <div class="editt">
+          
+        </div>
+        <div class="postflex">
+          <p class="loginparagraph">Created by:<b>`+ logg.postlogin +`</b></p>
+          <button id="edit" onclick="Edittxt()">EDIT</button>
+          <button id="click" onclick="Removepost(`+i+`)">DELETE POST</button>
+        </div>
+        
+      `
+      ;
+      if(localStorage.getItem("login")==logg.postlogin){
+        document.getElementsByClassName('postbox')[0].appendChild(pos);
+      } else {
+        continue;
+      }
+    }
+  }
+}
+
+function Removepost(input){
+  localStorage.removeItem(input);
+  window.open("author2.html","_self");
+}
+
+function Removeallpost(){
+  for (let i = 0; i < 9; i++){
+    if (localStorage.getItem(i) == null){
+      continue;
+    } else {
+      localStorage.removeItem(i);
+      window.open("author2.html","_self");
+    }
+  }
+}
+
+function Displayallposts(){
+  for (let i = 0; i < 9; i++){
+    if (localStorage.getItem(i) == null){
+      continue;
+    } else {
+      let log = localStorage.getItem(i);
+      let logg = JSON.parse(log);
+      var pos = document.createElement('div');
+      pos.className = 'rowp';
+
+      pos.innerHTML =`
+        <h2>`+ logg.posttitle +`</h2>
+        <p class="text">`+ logg.postinside +`</p>
+        <div class="postflex">
+          <p class="loginparagraph">Created by:<b>`+ logg.postlogin +`</b></p>
+        </div>
+        
+      `
+      ;
+      document.getElementsByClassName('postspace')[0].appendChild(pos);
+    }
+  }
+}
+function Displayallpostsmain(){
+  for (let i = 0; i < 9; i++){
+    if (localStorage.getItem(i) == null){
+      continue;
+    } else {
+      let log = localStorage.getItem(i);
+      let logg = JSON.parse(log);
+      var pos = document.createElement('div');
+      pos.className = 'rowm';
+
+      pos.innerHTML =`
+        <h2 class="t">`+ logg.posttitle +`</h2>
+        <p class="text">`+ logg.postinside +`</p>
+        <div class="postflex">
+          <p class="loginparagraph">Created by:<b>`+ logg.postlogin +`</b></p>
+        </div>
+        
+      `
+      ;
+      document.getElementsByClassName('thisposts')[0].appendChild(pos);
+    }
+  }
+}
+
+//document.addEventListener('DOMContentLoaded',init,false);
+
+
+/*
+function init(){
+
+    login = document.querySelector('#login');
+	password = document.querySelector('#password');
+
+}
+
+
+function logindata(login){
+
+}
+
+gfhlogin = 0;
+function isLogin(gfhlogin){
+    if (gfhlogin == 0){
+        window.open("author2.html","_self");
+    } else {
+        window.open("author.html","_self");
+    }
+}
+
+
+
+function addRow() {
+  
+    const div = document.createElement('div');
+
+    div.className = 'row';
+  
+    div.innerHTML = `
+      <input type="text" name="name" value="" />
+      <input type="text" name="value" value="" />
+      <label> 
+        <input type="checkbox" name="check" value="1" /> Checked? 
+      </label>
+      <input type="button" value="-" onclick="removeRow(this)" />
+    `;
+  
+    document.getElementById('postspace').appendChild(div);
+
+    
+    localStorage.setItem(div);
+  }
+
+  function addthis(){
+    const x = localStorage.getItem(div);
+    document.getElementById('postspace').appendChild(x);
+  }
+  
+  function removeRow(input) {
+    document.getElementById('postspace').removeChild(input.parentNode);
+  }
+  */
